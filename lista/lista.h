bd947afb134e12c449aaf3d7ef66804e0e4bbf1a
Lucas <guerra986@gmail.com>
2019-09-25 23:11:09 -0300

    dale

--- lista/lista.h
+++ lista/lista.h
@@ -43,6 +43,23 @@ size_t lista_largo(const lista_t *lista);       // ok
 void lista_destruir(lista_t *lista, void destruir_dato(void *));
 
 
+/* *****************************************************************
+ *                    PRIMITIVAS DEL ITERADOR EXTERNO
+ * *****************************************************************/
+
+lista_iter_t* lista_iter_crear (lista_t* lista);
+
+bool lista_iter_al_final (const lista_iter_t* iter);
+
+bool lista_iter_avanzar (lista_iter_t* iter);
+
+void* lista_iter_ver_actual (const lista_iter_t* iter);
+
+bool lista_iter_insertar (lista_iter_t* iter, void* dato);
+
+void* lista_iter_borrar (lista_iter_t* iter);
+
+void lista_iter_destruir (lista_iter_t* iter);
 
 
 /* *****************************************************************
