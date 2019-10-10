b728e77eb1b11ee781df1922151f39c716da6cc9
Lucas Guerra <lguerra@fi.uba.ar>
2019-10-10 14:05:53 -0300

    corriendo pruebas

--- hash/hash.c
+++ hash/hash.c
@@ -7,9 +7,10 @@
 
 #define CAP_MIN 47
 
-/* *****************************************************************
- *               Estructuras de la tabla y sus elementos
- * *****************************************************************/
+
+/* -----------------------------------------------------------------
+                Estructuras del Hash y sus elementos
+ * -----------------------------------------------------------------*/
 
 struct hash
 {
@@ -19,15 +20,23 @@ struct hash
     size_t capacidad;
 };
 
+
 typedef struct elemento_hash{
     char* clave;
     void* dato;
 } elemento_hash_t;
 
 
-/* *****************************************************************
- *               Funciones adicionales
- * *****************************************************************/
+struct hash_iter{
+    const hash_t* hash;
+    size_t indice;
+    lista_iter_t* iter_lista;
+};
+
+
+/* -----------------------------------------------------------------
+                Funciones adicionales
+ * -----------------------------------------------------------------*/
 
 char *strdup(const char *src) {
     char *dst = malloc(strlen (src) + 1);  // Space for length plus nul
@@ -38,9 +47,9 @@ char *strdup(const char *src) {
 
 
 
-/* *****************************************************************
- *                    Función de Hashing
- * *****************************************************************/
+/* -----------------------------------------------------------------
+             Funciones de Hashing
+ * -----------------------------------------------------------------*/
 size_t hashing(const char *clave, size_t tam){
 
     size_t hash = 5381;
@@ -54,9 +63,9 @@ size_t hashing(const char *clave, size_t tam){
 }
 
 
-/* *****************************************************************
- *                    Primitiva del elemento
- * *****************************************************************/
+/* -----------------------------------------------------------------
+             Primitivas del elemento
+ * -----------------------------------------------------------------*/
 
 elemento_hash_t* elemento_crear(char *clave, void* dato){
 
@@ -133,10 +142,19 @@ void* elemento_en_lista(const hash_t *hash,const char *clave){
     return dato;
 }
 
+/* -----------------------------------------------------------------
+                Funciones adicionales de la tabla de HASH
+ * -----------------------------------------------------------------*/
 
-/* *****************************************************************
- *                    Primitivas de la tabla de Hash
- * *****************************************************************/
+// Verifica si el hash está vacío
+bool hash_esta_vacio(const hash_t* hash){
+    if (hash->cantidad == 0) return true;
+    return false;
+}
+
+/* -----------------------------------------------------------------
+                Primitivas de la tabla de HASH
+ * -----------------------------------------------------------------*/
 
 hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 
@@ -195,10 +213,14 @@ void *hash_obtener(const hash_t *hash, const char *clave){
 }
 
 
-bool hash_pertenece(const hash_t *hash, const char *clave){
+/* bool hash_pertenece(const hash_t *hash, const char *clave){
     return elemento_en_lista(hash, clave);
-}
+} */
 
+bool hash_pertenece(const hash_t *hash, const char *clave){
+    elemento_hash_t* elemento = elemento_en_lista(hash, clave);
+    return elemento;
+}
 
 void *hash_borrar(hash_t *hash, const char *clave){
     return borrar_elemento(hash, clave);
@@ -223,4 +245,96 @@ void hash_destruir(hash_t *hash){
     }
     free(hash->listas);
     free(hash);
+}
+
+/* -----------------------------------------------------------------
+                Funciones adicionales del iterador
+ * -----------------------------------------------------------------*/
+
+size_t iter_buscar_indice(const hash_t* hash, size_t indice_actual){
+    
+    for (size_t i = indice_actual; i < hash->capacidad; i++){
+        if (!lista_esta_vacia(hash->listas[i])){
+            return i;
+        }
+    }
+    return hash->capacidad;
+}
+
+
+/* -----------------------------------------------------------------
+                Primitidas del iterador
+ * -----------------------------------------------------------------*/
+
+hash_iter_t *hash_iter_crear(const hash_t *hash){
+    
+    if (!hash) return NULL;
+    
+    hash_iter_t* iter = malloc(sizeof(hash_iter_t));
+    if (!iter) return NULL;
+
+    iter->hash = hash;
+    size_t inicial = 0;
+    
+    if (hash_esta_vacio(iter->hash)){
+        iter->indice = inicial;
+        iter->iter_lista = NULL;
+    }else{
+        iter->indice = iter_buscar_indice(hash, inicial);
+        iter->iter_lista = lista_iter_crear(iter->hash->listas[iter->indice]);
+        if(!iter->iter_lista) return NULL;
+    }
+    
+
+    return iter;
+}
+
+
+bool hash_iter_avanzar(hash_iter_t *iter){
+    // falta preguntar si no estoy en el ultimo elemento hash[capacidad] && iter_al_final
+    if (hash_esta_vacio(iter->hash)) return false;
+    
+    if (!lista_iter_al_final(iter->iter_lista)){
+        lista_iter_avanzar(iter->iter_lista);
+        return true;
+    }
+
+    lista_iter_destruir(iter->iter_lista);
+    iter->indice = iter_buscar_indice(iter->hash, iter->indice);
+
+    iter->iter_lista = lista_iter_crear(iter->hash->listas[iter->indice]);
+    if (!iter->iter_lista) return false;
+
+    return true;
+}
+
+
+const char *hash_iter_ver_actual(const hash_iter_t *iter){
+    
+    if (hash_iter_al_final(iter)) return NULL;
+
+    elemento_hash_t* aux = lista_iter_ver_actual(iter->iter_lista);
+    return aux->clave;
+}
+
+
+bool hash_iter_al_final(const hash_iter_t *iter){
+    
+    if (hash_esta_vacio(iter->hash)) return true;
+
+    if (iter->indice == iter->hash->capacidad){
+        if (lista_iter_al_final(iter->iter_lista) || iter->hash->listas[iter->indice] == NULL){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+void hash_iter_destruir(hash_iter_t* iter){
+    if (iter->iter_lista){
+        lista_iter_destruir(iter->iter_lista);
+    }
+    free(iter);
 }
